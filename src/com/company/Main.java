aa4cbf092d0ef9013d12e96ab79ee48caa1ef22d
Roman <rionik@mail.ru>
2019-06-26 22:31:08 +0600

    first commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String names[] = {"Иван", "Олег", "Антон"};
+        for (int i = 0; i < names.length; i++){
+            switch (i) {
+                case 0:
+                    System.out.println(names[i] + " доброго утра");
+                    break;
+                case 1:
+                    System.out.println(names[i] + " доброго дня");
+                    break;
+                case 2:
+                    System.out.println(names[i] + " доброго вечера");
+                    break;
+            }
+
+
+
+
+
+
+}
+
+    }
+}
