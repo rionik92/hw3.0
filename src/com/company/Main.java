8cc6153badf9582a6cf654e55a3c1a99c21cfd29
Roman <rionik@mail.ru>
2019-06-27 00:14:00 +0600

    +доп задание

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,14 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
         String names[] = {"Иван", "Олег", "Антон"};
+        names = Arrays.copyOf(names, names.length + 1);
+        names[3]="Саша";
         for (int i = 0; i < names.length; i++){
             switch (i) {
                 case 0:
@@ -16,14 +20,15 @@ public class Main {
                 case 2:
                     System.out.println(names[i] + " доброго вечера");
                     break;
+                case 3:
+                    System.out.println(names[i] + " доброй ночи");
+                    break;
+
+
             }
 
 
-
-
-
-
-}
+        }
 
     }
 }
